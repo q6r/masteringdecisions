16e4e9081254f1098c1f0f65a18f6e0e5d055a26
qnix <qnix@0x80.org>
2016-02-01 21:00:31 -0800

    CORS removed since we're hosting the frontend

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/astaxie/beego/config"
 	"github.com/gin-gonic/gin"
 	"github.com/go-gorp/gorp"
-	"github.com/itsjamie/gin-cors"
 	"github.com/rageix/ginAuth"
 )
 
@@ -26,19 +24,6 @@ func main() {
 	defer dbmap.Db.Close()
 
 	routes := gin.Default()
-
-	// Middlewares
-	/////////////////
-	// Apply the middleware to the router (works with groups too)
-	routes.Use(cors.Middleware(cors.Config{
-		Origins:         "*",
-		Methods:         "GET, PUT, POST, DELETE",
-		RequestHeaders:  "Origin, Authorization, Content-Type",
-		ExposedHeaders:  "",
-		MaxAge:          50 * time.Second,
-		Credentials:     false,
-		ValidateHeaders: false,
-	}))
 
 	// Debug routes
 	/////////////////
