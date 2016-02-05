e84e9d27fd2487669ecdd45a1decf178631d5de2
jln2 <jln2@pdx.edu>
2016-02-04 21:50:32 -0800

    added ballot retrieval to page_ballot.js, checks decision stage

--- static/page_ballot.js
+++ static/page_ballot.js
@@ -1,8 +1,11 @@
+var base_url = "http://localhost:9999";
+
 function main(body)
 {
 	$('title').html('Ballot');
 	$('head').prepend(custom_header());
 
+	
 	var ballot_info = getBallotCookies();
 
 	if(ballot_info == null || ballot_info == undefined) {
@@ -12,8 +15,88 @@ function main(body)
 		var decision_id = ballot_info[0];
 		var ballot_id = ballot_info[1];
 
+		var decision_name;
+		var decision_desc;
+		var decision_stage;
+		var criterion_names = [];
+		var criterion_descriptions = [];
+		var alternative_names = [];
+		var alternative_descriptions = [];
+
+
 		$("body")
-		.append("Decision_id: "+decision_id+"<br>"+"Ballot_id: "+ballot_id);
+		.append("Decision_id: "+decision_id+"<br>"+"Ballot_id: "+ballot_id+"<br>");
+		
+		get_text("/decision/"+decision_id+"/info", function(dec) {
+
+			decision_name = dec.decision.name;
+			decision_desc = dec.decision.description;
+			decision_stage = dec.decision.stage;
+
+			if(decision_stage == "1") {
+				$("body")
+				.append("<h1>Voting has not started yet, please check back later</h1>");
+				
+				return;
+			}
+			else if (decision_stage == "3") {
+				$("body")
+				.append("<h1>Voting has been closed!</h1>");
+
+				return;
+			}
+			else {
+
+				$("body")
+				.append("name: "+decision_name+"<br>"+"desc: "+decision_desc+"<br>"+"stage: "+decision_stage+"<br>");
+
+				get_text("/decision/"+decision_id+"/criterions", function (crits) {
+
+					for(var i=0; i<crits.criterions.length; i++) {
+
+						criterion_names[i] = crits.criterions[i].name;
+						criterion_descriptions[i] = crits.criterions[i].description;
+
+					}
+
+					for(var j=0; j<crits.criterions.length; j++) {
+
+						$("body")
+						.append("Crit "+j+" name: "+criterion_names[j]+"<br>");
+
+						$("body")
+						.append("Crit "+j+" desc: "+criterion_descriptions[j]+"<br>");
+
+					}
+			
+					get_text("/decision/"+decision_id+"/alternatives", function(alts) {
+
+						for(var i=0; i<alts.alternatives.length; i++) {
+
+							alternative_names[i] = alts.alternatives[i].name;
+							alternative_descriptions[i] = alts.alternatives[i].description;
+				
+						}
+	
+						for(var j=0; j<alts.alternatives.length; j++) {
+							
+							$("body")
+							.append("Alt "+j+" name: "+alternative_names[j]+"<br>");
+
+							$("body")
+							.append("Alt "+j+" desc: "+alternative_descriptions[j]+"<br>");
+						}	
+
+					});
+
+				});
+				
+
+
+			}
+
+		});
+
 	}
 }
 
@@ -47,4 +130,32 @@ function getBallotCookies() {
 function custom_header() {
 	var content='<link rel="stylesheet" type="text/css" href="/static/css/ballot.css">'
 	return content
+}
+
+get_text = function(url, cb) {
+	var request = new XMLHttpRequest();
+	request.open('GET', base_url+url, true);
+	request.setRequestHeader("Content-Type", "application/json");
+
+	request.onreadystatechange = function() {
+		if(request.readyState == 4 && request.status == 200) {
+			cb(JSON.parse(request.responseText));
+		}
+	}
+
+	request.send();
+}
+
+post_text = function(url, data, cb) {
+	var request = new XMLHttpRequest();
+	request.open('POST', base_url+url, true);
+	request.setRequestHeader("Content-Type", "application/json");
+
+	request.onreadystatechange = function() {
+		if(request.readyState == 4 && request.status == 200) {
+			cb(JSON.parse(request.responseText));
+		}
+	}
+
+	request.send(data);
 }
