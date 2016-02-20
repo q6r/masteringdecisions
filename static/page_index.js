cffede0b212853ed44c938e2dd7eb27393fc7845
JasonH2o <jasonliu820@hotmail.com>
2016-02-19 23:10:29 -0800

    user can sort the table by ballot status

--- static/page_index.js
+++ static/page_index.js
@@ -1187,7 +1187,7 @@ function buildHome() {
 	  '<tr>',
 	  '<th>Name</th>',
 	  '<th>Email</th>',
-	  '<th>Ballot status</th>',
+	  '<th id="status_header">Ballot status</th>',
 	  '<th>Action</th>',
 	  '<th>Reset</th>',
 	  '<th></th>',
@@ -1204,6 +1204,7 @@ function buildHome() {
     $('#s_table').hide()
 
     getStatus(decisionID)
+	sortTable()
     }
   
     function getStatus(decisionID){
@@ -1362,6 +1363,25 @@ function buildHome() {
 	  $('#ballotEmail').val(ballot.email)
 	}
 	
+	//functions to sort the table by ballot status
+	function sortTable(){
+		$('#status_header').click(function(){
+    	var table = $(this).parents('table').eq(0)
+    	var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    	this.asc = !this.asc
+    	if (!this.asc){rows = rows.reverse()}
+    	for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+		})
+	}	
+
+	function comparer(index) {
+    	return function(a, b) {
+        	var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        	return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+    	}
+	}
+	
+	function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
 
   /**** Decision Invite ****/
     function buildDecisionInvite(decisionID){
