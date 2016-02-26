40723cd8ebcf632bd6a6463ee68de4a215e370a9
jln2 <jln2@pdx.edu>
2016-02-25 21:35:27 -0800

    initial results page. code is ugly, don't look

--- static/page_results.js
+++ static/page_results.js
@@ -1,301 +1,405 @@
-var base_url = "http://localhost:9999";
+var decision_name;
+var criterion_names = [];
+var criterion_ids = [];
+var alternative_names = [];
+var alternative_ids = [];
+var ballots;
 
-function main(body)
-{
+
+function main(body) {
+
+	var decision_id = window.location.pathname.substr('/results/'.length);
+	var decision_stage;
+
 	$('title').html('Results');
 	$('head').prepend(custom_header());
 
-	
-	var decision_id = getDecisionId();
+	var decision = get_decision(decision_id);
 
-	if(decision_id == null || decision_id == undefined) {
-		$("body")
-		.append("<h1>Error: No ballot found.  Please make sure cookies are enabled and try the link again</h1>");	
-	} else {
-                //Varible List
-                //Ballot Varibles
-                var bname = undefined;
-                var ratings = [];
-                var votes = [];
-                var url = undefined;
-                var email = undefined;
-                //This should look like
-                //  [Crit_Id, [Weight,], [Alternative_Id, [Rating,]],]
-                var info_set = [];
-                //This should look like
-    		// [[Crit_id, Weighted Avg, [Alternative_id, weighted_sum, total]]]
-		var weighted_set = [];
-
-		//$("body")
+	if(decision == null || decision["error"] != null || decision["error"] != undefined) {
+		alert("Unable to get decision information");
+		return;
+	}
 		
-		get_text("/decision/"+decision_id+"/ballots", function(dec) {
+	ballots = get_ballots(decision_id);
 
-			//Pull out info from Ballots
-                        bname = dec.name;
-                        votes = dec.votes;
-                        ratings = dec.rating;
-                        email = dec.email;
-                        url = dec.url;
-                        
-			info_set = initPairedList(ratings, votes);
-                        //Match up all the votes and ratings with criteron
-			weighted_set = buildWeightedSums(info_set);
-                        
-//------------------------
-			if(decision_stage == "1") {
-				$("body")
-				.append("<h1>Voting has not started yet, please check back later</h1>");
-				
-				return;
-			}
-			else if (decision_stage == "3") {
-				$("body")
-				.append("<h1>Voting has been closed!</h1>");
-
-				return;
-			}
-			else if (decision_stage == "4") {
-				$("body")
-				.append("<h1>This decision has been locked.</h1>");
-
-				return;
-			}
-			else {
-
-				//$("body")
-				//.append("name: "+decision_name+"<br>"+"desc: "+decision_desc+"<br>"+"stage: "+decision_stage+"<br>");
-
-				get_text("/decision/"+decision_id+"/criterions", function (ballot_list) {
-
-					for(var i=0; i<crits.criterions.length; i++) {
-
-						criterion_names[i] = crits.criterions[i].name;
-						criterion_descriptions[i] = crits.criterions[i].description;
-
-					}
-					//PAGE
-
-			
-						//PAGE REFERENCE	
-						var page="<div id=\"topbar\" class=\"navbar navbar-default navbar-fixed-top\">"
-						+"<div class=\"container\">"
-						+"<a class=\"navbar-brand\">"+decision_name+"</a>"
-						+"</div>"
-						+"</div>"
-						+"<div id=\"ballotbody\" class=\"container\">"
-						+"<div class=\"row\">"
-						+"<div class=\"col-md-6 col-md-offset-3\" id=\"topRow\">"
-						+"<p class=\"lead\">"+decision_desc+"</p>"
-						+"<p>Instructions for criterion</p>"
-						+"<p>Click on a criterion name for description</p>"
-						+"</div>"
-						+"</div>"
-						+"<form class=\"form-horizontal\" role=\"form\">";
-
-						for(var i=0; i<criterion_names.length; i++) {
-						
-							page+="<div class=\"row\">"
-							+"<div class=\"form-group\">"
-							+"<label id=crit"+i+" class=\"col-sm-4 control-label criterion\">"+criterion_names[i]+"</label>"
-							+"<div class=\"col-sm-8 btn-group\" data-toggle=\"buttons\">";
-
-							for(var j=1; j<11; j++) {
-
-								page+="<label class=\"btn btn-default votebtn\">"+"<input type=\"radio\" name=\"crit"+parseInt(i)+"\" id=\"crit"+i+"_"+j+"\" value=\""+j+"\">"+j+"</label>";
-
-
-							}
-
-							page+="</div>"
-							+"<div class=\"alert alert-success center\" id=\"crit"+i+"Desc\">"+criterion_descriptions[i]+"</div>"
-							+"</div>"
-							+"</div>";
-			
-						}
-
-						page+="</form>"
-						+"</div>";
-
-						$("body").append(page);
-
-						
-					});
-
-				});
-				
-
-
-			}
-a
-		});
-
-
-
-
+	if(ballots == null || ballots["error"] != null || ballots["error"] != undefined) {
+		alert("Unable to get ballot information");
+		return;
 	}
-}
-//---------------------------------------
-//Set up the Paired_List
-//Note: Rating and Weight have been named backwards on Backend so it looks backwards within this function
-function initPairedList(ratings, votes){
-	//Local Varibles
-        var ret = [];
 
-        var flag1 = 0;
-	var flag2 = 0;
-	var flag3 = 0;
+	var criterion = get_criterion(decision);
 
-	//Initialize all
-        //First: votes array
-	for (i = 0; i < votes.length; ++i){
-		for (k = 0; k < ret.length; ++k){
-			if (ret[k][0] == votes[i].criterion_id){
-				flag1 = 1;
-				for (j = 0; j < ret[k][2].length){
-					if (ret[k][2][j][1] == votes[i].alternative_id){
-						flag2 = 1;
-						ret[k][2][j][1].push(votes[i].weight);
-					}
-				}
-				if (flag2 == 0){
-					ret[k][2].push([votes[i].alternative_id, [votes[i].weight]]);
-				}
-				else{
-					flag2 = 0;
-				}
-			}
+	if(criterion == null || criterion["error"] != null || criterion["error"] != undefined) {
+		alert("Unable to get criterion information");
+		return;
+	}
+
+	var alternatives = get_alternatives(decision);
+		
+	if(alternatives == null || alternatives["error"] != null || alternatives["error"] != undefined) {
+		alert("Unable to get alternative information");
+		return;
+	}
+
+	decision_stage = decision.stage;
+
+	if(decision_stage != "3") {
+
+		$("body").append("<h1>Decision stage must be complete in order to view results</h1>");
+		return;
+
+	} else {
+		
+		decision_name = decision.name;
+
+		//criterion info
+		for(var i=0; i<criterion.length; i++) {
+			criterion_names[i] = criterion[i].name;
+			criterion_ids[i] = criterion[i].criterion_id;		
+
 		}
-                if (flag1 == 0){
-			ret.push([votes[i].criterion_id, [], [votes[i].alternative_id, [votes[i].weight]]]);
-                }
-                else{
-			flag1 = 0;
-        	}
-	}
-	//Second: rating array
-	for (h = 0; h < ratings.length; ++h){
-		for (g = 0; g < ret.length; ++g){
-			if (ret[g][0] == ratings[h].criterion_id){
-				flag3 = 1;
-				ret[g][1].put(rating[h].rating);
-			}
-		}
-                if (flag3 == 0){
-			ret.push([ratings[h].criterion_id, [rating[h].rating], []]);
-                }
-                else{
-			flag3 = 0;
-        	}
 
-	}
+		//alternative info
+		for(var i=0; i<alternatives.length; i++) {
 	
-	return ret;
+			alternative_names[i] = alternatives[i].name;
+			alternative_ids[i] = alternatives[i].alternative_id;
+	
+		}
 
-}
+		google.charts.load('current', {'packages':['corechart', 'table'], 'callback':wait()});
 
-//Input
-//[[Crit_Id, [Weight,], [Alternative_Id, [Rating,],]],]
-//Output
-//[Crit_id, Weighted Avg, Weighted Total, [Alternative_id, weighted_sum, total_sum]]]
-function buildWeightedSums(info_set){
-	//Local Variables
-	var ret = [];
-	var weight = undefined;
-	var weightedSums = [];
-        var z = 0;
-        var weigth_total = 0;
+		var page = '<table align="center">'
+		+'<tr align="top">'
+		+'<td colSpan=2>'
+		+'<div id="chart_div"></div>'
+		+'</td>'
+		+'</tr>'
+		+'<tr>'
+		+'<td>'
+		+'<div id="table_div"></div>'
+		+'</td>'
+		+'</tr>'
+		+'</table>';
 
-	//Build Weights
-	for (z = 0; z < info_set.length; ++z){
-                weight = mathWeight(into_set[z][1], weight_total);
-                weight_total = weight_total + weight[1];
-		weightedSums = mathNormalized(into_set[z][2], weight[0]);
-		ret.push([into_set[z][0], weight[0], weight[1], weightedSums]);
-	}
-}
+		$('body').append(page);
+	
 
-function mathWeight(weight_list, total){
-	//Local Variables
-  	var ret = [];
-        var avg  = 0;
-	var i = 0;
-
-	//Build Average Weight
-  	for(i = 0; i < weight_list.length; ++i){
-		avg = avg + weigth_list;
-  	}
-        if( avg == 0 ){
-		ret = [0, total];
-		return ret;
- 	}
-        total = total + avg
-	avg = avg / i;
-	ret = [avg, total];
-	return ret;
-}
-
-function mathNormalized(alt_list, weight ){
-        if (alt_list == undefined){
-		return undefined;
-        }
-	//Local Variables
-	var ret = [];
-        var alt_id = alt_list[0];
-        var value = 0;
-        var avg = 0;
-	var i = 0;
-
-	//Build Normalized Ratings
-	for (i = 0; i < alt_list[1].length; ++i){
-		value = value + alt_list[1][i];
-	}
-        avg = value / i;
-        ret = [alt_id, avg, value];
-	return ret;
-}
-
-function getDecisionId() {
-
-	var my_cookies = document.cookie.split(';')
-
-	if(my_cookies == "") {
-		return null;
 	}
 
-	var decision_id = my_cookies[0].split('=');
 
-	return decision_id;
+
 }
+
 
 function custom_header() {
-	var content='<link rel="stylesheet" type="text/css" href="/static/css/ballot.css">'
-	return content;
+	var content='<link rel="stylesheet" type="text/css" href="/static/css/results.css">'
+	return content
 }
 
-get_text = function(url, cb) {
-	var request = new XMLHttpRequest();
-	request.open('GET', base_url+url, true);
-	request.setRequestHeader("Content-Type", "application/json");
-
-	request.onreadystatechange = function() {
-		if(request.readyState == 4 && request.status == 200) {
-			cb(JSON.parse(request.responseText));
+//retrieve decision 
+function get_decision(decision_id) {
+	var result = null;
+	$.ajax({
+		type: "GET",
+		url: base_url+"/decision/"+decision_id+"/info",
+		contentType: 'application/json; charset=utf-8',
+		async: false,
+		success: function (r) {
+			result = r["decision"];
+		},
+		error: function (r) {
+			errmsg = JSON.parse(r.responseText);
+			result = errmsg;
 		}
+	});
+	return result;
+}
+function wait() {
+
+	setTimeout(function() {drawTable();}, 3000);
+
+}
+
+function drawTable() {
+
+        var data = new google.visualization.DataTable();
+	
+
+	var weight_row = [];
+	var temp_sum = 0.0;
+
+	var weights = [];
+	var final_weights = [];
+	var weight_total = 0;
+	var ballot_weights = [];
+	var temp_weights = [];
+
+	var votes = [];
+	var temp_votes = [];
+	var ballot_votes = [];
+	var final_votes = [];
+	var vote_total = [];
+
+	var tally = [];
+	var final_tally = [];
+
+
+	for(var i=0; i<ballots.length; i++) {
+
+		temp_sum = 0;
+		temp_weights = [];
+
+		for(var j=0; j<ballots[i].rating.length; j++) {
+
+			temp_weights.push(ballots[i].rating[j].rating);
+			temp_sum += ballots[i].rating[j].rating;
+
+
+		}
+		
+		weights[i] = new Array();
+
+		for(var j=0; j<temp_weights.length; j++) {
+
+			weights[i].push(temp_weights[j]/temp_sum);
+
+		}
+		
+
 	}
 
-	request.send();
-}
 
-post_text = function(url, data, cb) {
-	var request = new XMLHttpRequest();
-	request.open('POST', base_url+url, true);
-	request.setRequestHeader("Content-Type", "application/json");
+	for(var i=0; i<weights[0].length; i++) {
 
-	request.onreadystatechange = function() {
-		if(request.readyState == 4 && request.status == 200) {
-			cb(JSON.parse(request.responseText));
+		temp_sum = 0;
+		temp_weights = [];
+
+		for(var j=0; j<weights.length; j++) {
+
+			temp_sum += weights[j][i];
 		}
+		
+		final_weights.push(temp_sum/weights.length);
+
+	}	
+	
+	for(var i=0; i<final_weights.length; i++) {
+		weight_total += final_weights[i];
 	}
 
-	request.send(data);
+
+	for(var i=0; i<ballots.length; i++) {
+
+		temp_sum = 0;
+		temp_votes = [];	
+
+
+		for(var j=0; j<ballots[i].votes.length; j++) {
+
+			temp_votes.push(ballots[i].votes[j].weight);
+			temp_sum += ballots[i].votes[j].weight;
+
+		}
+
+
+		votes[i] = new Array();
+	
+		for(var j=0; j<temp_votes.length; j++) {
+			
+			votes[i].push(temp_votes[j]);	
+
+		}
+
+	}
+
+
+	for (var i=0; i<votes[0].length; i++) {
+
+		temp_sum=0;
+		temp_votes = [];
+
+
+		for(var j=0; j<votes.length; j++) {
+
+			temp_sum += votes[j][i];
+
+		}
+
+		final_votes.push(temp_sum/votes.length);
+
+	}
+
+	var counter=0;
+
+	for(var i=0; i<alternative_names.length; i++) {
+
+		tally[i] = new Array();
+
+		for (var j=0; j<criterion_names.length; j++) {
+
+			tally[i].push(final_votes[counter]);
+			counter++;
+		}
+
+	}
+
+	var chart_junk = [];
+
+	for(var i=0; i<tally.length; i++) {
+
+		temp_sum = 0;
+
+		for(var j=0; j<tally[0].length; j++) {
+			chart_junk.push(tally[i][j] * final_weights[j]);
+			temp_sum += (tally[i][j] * final_weights[j]);
+		}
+
+		final_tally.push(temp_sum);
+
+	}
+
+        data.addColumn('string', '');
+	
+	for(var i=0; i<criterion_names.length; i++) {
+
+		data.addColumn('number', criterion_names[i]);
+
+	}
+
+        data.addColumn('number', 'Final Tally');
+
+	weight_row = ['weights'];
+
+	for(var i=0; i<criterion_names.length; i++) {
+
+		weight_row.push(final_weights[i] * 100);
+
+	}
+	
+	weight_row.push(weight_total * 100);
+
+	data.addRow(weight_row);
+
+	counter = 0;
+	var chart_rows = [];
+
+	for(var i=0; i<alternative_names.length; i++) {
+		
+		var row = [alternative_names[i]];
+		chart_rows[i] = new Array();
+
+		for(var j=0; j<criterion_names.length; j++) {
+		
+			row.push(final_votes[counter]);
+			counter++;
+		}
+
+		row.push(final_tally[i]);
+		data.addRow(row);
+
+	}
+
+        var table = new google.visualization.Table(document.getElementById('table_div'));
+
+        table.draw(data, {showRowNumber: false, width: '100%', height: '100%'});
+
+
+
+        var data_c = new google.visualization.DataTable();
+
+	data_c.addColumn('string', 'criterion');
+
+	for(var i=0; i<criterion_names.length; i++) {
+
+		data_c.addColumn('number', criterion_names[i]);
+
+	}
+
+	counter = 0;
+
+	chart_rows = [];
+
+	for(var i=0; i<alternative_names.length; i++) {
+
+		var row = [alternative_names[i]];
+		chart_rows[i] = new Array();
+
+		for(var j=0; j<criterion_names.length; j++) {
+
+			row.push(chart_junk[counter]);
+			counter++;
+
+		}
+				
+		data_c.addRow(row);	
+	}	
+
+	new google.visualization.ColumnChart(document.getElementById('chart_div')).
+		draw(data_c,
+		{title:"Voting Results - "+decision_name,
+		width:600, height:400,
+		vAxis: {title: "Votes"}, isStacked: true}
+	);
+
 }
+
+
+//retrieve all ballots
+function get_ballots(decision_id) {
+	var result = null;
+	$.ajax({
+		type: "GET",
+		url: base_url+"/decision/"+decision_id+"/ballots",
+		contentType: 'application/json; charset=utf-8',
+		async: false,
+		success: function (r) {
+			result = r["ballots"];
+		},
+		error: function (r) {
+			errmsg = JSON.parse(r.responseText);
+			result = errmsg;
+		}
+	});
+	return result;
+}
+
+//retrieve criterion
+function get_criterion(decision) {
+	var result = null;
+	$.ajax({
+		type: "GET",
+		url: base_url+"/decision/"+decision["decision_id"]+"/criterions",
+		contentType: 'application/json; charset=utf-8',
+		async: false,
+		success: function (r) {
+			result = r["criterions"];
+		},
+		error: function (r) {
+			errmsg = JSON.parse(r.responseText);
+			result = errmsg;
+		}
+	});
+	return result;
+}
+
+//retrieve alternatives
+function get_alternatives(decision) {
+	var result = null;
+	$.ajax({
+		type: "GET",
+		url: base_url+"/decision/"+decision["decision_id"]+"/alternatives",
+		contentType: 'application/json; charset=utf-8',
+		async: false,
+		success: function (r) {
+			result = r["alternatives"];
+		},
+		error: function (r) {
+			errmsg = JSON.parse(r.responseText);
+			result = errmsg;
+		}
+	});
+	return result;
+}
+
