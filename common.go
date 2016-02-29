e17027fc9d31ea055739a72dd7a837989ae15c1c
qnix <qnix@0x80.org>
2016-02-29 12:43:48 -0800

    Removed unnecessary header check

--- common.go
+++ common.go
@@ -7,14 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// ServeResults serve the results at the end
+// ServeResult serve the results at the end
 // of most handlers
 func ServeResult(c *gin.Context, scriptName string, result gin.H) {
-	if strings.Contains(c.Request.Header.Get("Content-Type"), "application/json; charset=utf-8") {
+	if strings.Contains(c.Request.Header.Get("Content-Type"), "application/json") {
 		c.JSON(http.StatusOK, result)
-	} else if strings.Contains(c.Request.Header.Get("Content-Type"), "application/json") {
-		c.JSON(http.StatusOK, result)
-	}else {
+	} else {
 		c.HTML(http.StatusOK, "htmlwrapper.tmpl",
 			gin.H{"scriptname": scriptName, "body": result})
 	}
